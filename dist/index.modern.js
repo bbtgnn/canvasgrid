5b5e23c440b64d5a4646c5893467006ede7288b9
bbtgnn <30571828+bbtgnn@users.noreply.github.com>
2021-03-29 13:09:35 +0200

    removed minification in package.json

--- dist/index.modern.js
+++ dist/index.modern.js
@@ -1,2 +1,175 @@
-function t(t,i=2){const s=Math.pow(10,i);return Math.round(t*s)/s}class i{constructor(i,s){this.x=t(i),this.y=t(s)}add(t){return new i(this.x+t.x,this.y+t.y)}smult(t){return new i(this.x*t,this.y*t)}vscale(t,s=new i(0,0)){return new i(s.x+(this.x-s.x)*t.x,s.y+(this.y-s.y)*t.y)}sscale(t,s=new i(0,0)){return this.vscale(new i(t,t),s)}}class s{constructor(i,s){this.width=t(i),this.height=t(s)}get ratio(){return this.width/this.height}fitSize(t){let i,e;return this.ratio>t?(e=this.height,i=e*t):(i=this.width,e=i/t),new s(i,e)}}class e{constructor(t,i){this.origin=t,this.size=i}get x(){return this.origin.x}get y(){return this.origin.y}get width(){return this.size.width}get height(){return this.size.height}get ratio(){return this.size.ratio}translate(t){return new e(this.origin.add(t),this.size)}fitRectangleCenter(t){const s=this.size.fitSize(t);return new e(new i(this.origin.x+(this.width-s.width)/2,this.origin.y+(this.height-s.height)/2),s)}}class h extends e{constructor(t,i,s){super(t,i),this.index=s}get i(){return this.index.i}get j(){return this.index.j}}class r{constructor(t,i,s=1,e={column:0,row:0}){this.rows=t,this.columns=i,this.cell_ratio=s,this.spacing=e}get height(){return 1*this.rows+(this.rows-1)*this.spacing.row}get width(){return this.columns*this.cell_ratio+(this.columns-1)*this.spacing.column}get ratio(){return this.width/this.height}getCellHeightFromGridHeight(t){return t/this.height}getCellHeightFromGridWidth(t){return t/this.width}getCells(t=1,e=new i(0,0)){const r=[];for(let n=0;n<this.rows;n++){const o=n*(1+this.spacing.row);for(let c=0;c<this.columns;c++){const g=new i(c*(this.cell_ratio+this.spacing.column),o).sscale(t).add(e);r.push(new h(g,new s(t*this.cell_ratio,t),{i:n,j:c}))}}return r}}export{h as Cell,i as Point,e as Rectangle,s as Size,r as UnitGrid};
+function round(v, d = 2) {
+  const p = Math.pow(10, d);
+  return Math.round(v * p) / p;
+}
+
+class Point {
+  constructor(x, y) {
+    this.x = round(x);
+    this.y = round(y);
+  }
+
+  add(point) {
+    return new Point(this.x + point.x, this.y + point.y);
+  }
+
+  smult(value) {
+    return new Point(this.x * value, this.y * value);
+  }
+
+  vscale(scale, origin = new Point(0, 0)) {
+    return new Point(origin.x + (this.x - origin.x) * scale.x, origin.y + (this.y - origin.y) * scale.y);
+  }
+
+  sscale(scale, origin = new Point(0, 0)) {
+    return this.vscale(new Point(scale, scale), origin);
+  }
+
+}
+class Size {
+  constructor(width, height) {
+    this.width = round(width);
+    this.height = round(height);
+  }
+
+  get ratio() {
+    return this.width / this.height;
+  }
+
+  fitSize(ratio) {
+    let width;
+    let height; //
+
+    if (this.ratio > ratio) {
+      height = this.height;
+      width = height * ratio;
+    } //
+    else {
+        width = this.width;
+        height = width / ratio;
+      }
+
+    return new Size(width, height);
+  }
+
+}
+class Rectangle {
+  constructor(origin, size) {
+    this.origin = origin;
+    this.size = size;
+  } // Simple shortcuts
+
+
+  get x() {
+    return this.origin.x;
+  }
+
+  get y() {
+    return this.origin.y;
+  }
+
+  get width() {
+    return this.size.width;
+  }
+
+  get height() {
+    return this.size.height;
+  }
+
+  get ratio() {
+    return this.size.ratio;
+  } //
+
+
+  translate(vector) {
+    return new Rectangle(this.origin.add(vector), this.size);
+  }
+
+  fitRectangleCenter(ratio) {
+    // Getting the base size
+    const size = this.size.fitSize(ratio); // Then we calculate its new x & y
+
+    const x = this.origin.x + (this.width - size.width) / 2;
+    const y = this.origin.y + (this.height - size.height) / 2; //
+
+    return new Rectangle(new Point(x, y), size);
+  }
+
+}
+class Cell extends Rectangle {
+  constructor(origin, size, index) {
+    super(origin, size);
+    this.index = index;
+  }
+
+  get i() {
+    return this.index.i;
+  }
+
+  get j() {
+    return this.index.j;
+  }
+
+}
+class UnitGrid {
+  constructor(rows, columns, cell_ratio = 1, spacing = {
+    column: 0,
+    row: 0
+  }) {
+    this.rows = rows;
+    this.columns = columns;
+    this.cell_ratio = cell_ratio;
+    this.spacing = spacing;
+  }
+
+  get height() {
+    /**
+     * number of cells in a column   * cell height +
+     * number of gaps between colums * gap height
+     */
+    return this.rows * 1 + (this.rows - 1) * this.spacing.row;
+  }
+
+  get width() {
+    /**
+     * number of cell in row        * cell width +
+     * number of gaps between rows  * gap width
+     */
+    return this.columns * this.cell_ratio + (this.columns - 1) * this.spacing.column;
+  }
+
+  get ratio() {
+    return this.width / this.height;
+  }
+
+  getCellHeightFromGridHeight(height) {
+    return height / this.height;
+  }
+
+  getCellHeightFromGridWidth(width) {
+    return width / this.width;
+  }
+
+  getCells(cell_height = 1, translation = new Point(0, 0)) {
+    const cells = [];
+
+    for (let r = 0; r < this.rows; r++) {
+      const y = r * (1 + this.spacing.row);
+
+      for (let c = 0; c < this.columns; c++) {
+        const x = c * (this.cell_ratio + this.spacing.column);
+        const origin = new Point(x, y).sscale(cell_height).add(translation);
+        cells.push(new Cell(origin, new Size(cell_height * this.cell_ratio, cell_height), {
+          i: r,
+          j: c
+        }));
+      }
+    }
+
+    return cells;
+  }
+
+}
+
+export { Cell, Point, Rectangle, Size, UnitGrid };
 //# sourceMappingURL=index.modern.js.map
--- dist/index.umd.js
+++ dist/index.umd.js
@@ -1,2 +1,283 @@
-!function(t,i){"object"==typeof exports&&"undefined"!=typeof module?i(exports):"function"==typeof define&&define.amd?define(["exports"],i):i((t||self).canvasgrid={})}(this,function(t){function i(t,i){for(var n=0;n<i.length;n++){var e=i[n];e.enumerable=e.enumerable||!1,e.configurable=!0,"value"in e&&(e.writable=!0),Object.defineProperty(t,e.key,e)}}function n(t,n,e){return n&&i(t.prototype,n),e&&i(t,e),t}function e(t,i){return(e=Object.setPrototypeOf||function(t,i){return t.__proto__=i,t})(t,i)}function r(t,i){void 0===i&&(i=2);var n=Math.pow(10,i);return Math.round(t*n)/n}var o=function(){function t(t,i){this.x=r(t),this.y=r(i)}var i=t.prototype;return i.add=function(i){return new t(this.x+i.x,this.y+i.y)},i.smult=function(i){return new t(this.x*i,this.y*i)},i.vscale=function(i,n){return void 0===n&&(n=new t(0,0)),new t(n.x+(this.x-n.x)*i.x,n.y+(this.y-n.y)*i.y)},i.sscale=function(i,n){return void 0===n&&(n=new t(0,0)),this.vscale(new t(i,i),n)},t}(),h=function(){function t(t,i){this.width=r(t),this.height=r(i)}return t.prototype.fitSize=function(i){var n,e;return this.ratio>i?n=(e=this.height)*i:e=(n=this.width)/i,new t(n,e)},n(t,[{key:"ratio",get:function(){return this.width/this.height}}]),t}(),s=function(){function t(t,i){this.origin=t,this.size=i}var i=t.prototype;return i.translate=function(i){return new t(this.origin.add(i),this.size)},i.fitRectangleCenter=function(i){var n=this.size.fitSize(i);return new t(new o(this.origin.x+(this.width-n.width)/2,this.origin.y+(this.height-n.height)/2),n)},n(t,[{key:"x",get:function(){return this.origin.x}},{key:"y",get:function(){return this.origin.y}},{key:"width",get:function(){return this.size.width}},{key:"height",get:function(){return this.size.height}},{key:"ratio",get:function(){return this.size.ratio}}]),t}(),u=function(t){var i,r;function o(i,n,e){var r;return(r=t.call(this,i,n)||this).index=e,r}return r=t,(i=o).prototype=Object.create(r.prototype),i.prototype.constructor=i,e(i,r),n(o,[{key:"i",get:function(){return this.index.i}},{key:"j",get:function(){return this.index.j}}]),o}(s),c=function(){function t(t,i,n,e){void 0===n&&(n=1),void 0===e&&(e={column:0,row:0}),this.rows=t,this.columns=i,this.cell_ratio=n,this.spacing=e}var i=t.prototype;return i.getCellHeightFromGridHeight=function(t){return t/this.height},i.getCellHeightFromGridWidth=function(t){return t/this.width},i.getCells=function(t,i){void 0===t&&(t=1),void 0===i&&(i=new o(0,0));for(var n=[],e=0;e<this.rows;e++)for(var r=e*(1+this.spacing.row),s=0;s<this.columns;s++){var c=new o(s*(this.cell_ratio+this.spacing.column),r).sscale(t).add(i);n.push(new u(c,new h(t*this.cell_ratio,t),{i:e,j:s}))}return n},n(t,[{key:"height",get:function(){return 1*this.rows+(this.rows-1)*this.spacing.row}},{key:"width",get:function(){return this.columns*this.cell_ratio+(this.columns-1)*this.spacing.column}},{key:"ratio",get:function(){return this.width/this.height}}]),t}();t.Cell=u,t.Point=o,t.Rectangle=s,t.Size=h,t.UnitGrid=c});
+(function (global, factory) {
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+  typeof define === 'function' && define.amd ? define(['exports'], factory) :
+  (global = global || self, factory(global.canvasgrid = {}));
+}(this, (function (exports) {
+  function _defineProperties(target, props) {
+    for (var i = 0; i < props.length; i++) {
+      var descriptor = props[i];
+      descriptor.enumerable = descriptor.enumerable || false;
+      descriptor.configurable = true;
+      if ("value" in descriptor) descriptor.writable = true;
+      Object.defineProperty(target, descriptor.key, descriptor);
+    }
+  }
+
+  function _createClass(Constructor, protoProps, staticProps) {
+    if (protoProps) _defineProperties(Constructor.prototype, protoProps);
+    if (staticProps) _defineProperties(Constructor, staticProps);
+    return Constructor;
+  }
+
+  function _inheritsLoose(subClass, superClass) {
+    subClass.prototype = Object.create(superClass.prototype);
+    subClass.prototype.constructor = subClass;
+
+    _setPrototypeOf(subClass, superClass);
+  }
+
+  function _setPrototypeOf(o, p) {
+    _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) {
+      o.__proto__ = p;
+      return o;
+    };
+
+    return _setPrototypeOf(o, p);
+  }
+
+  function round(v, d) {
+    if (d === void 0) {
+      d = 2;
+    }
+
+    var p = Math.pow(10, d);
+    return Math.round(v * p) / p;
+  }
+
+  var Point = /*#__PURE__*/function () {
+    function Point(x, y) {
+      this.x = round(x);
+      this.y = round(y);
+    }
+
+    var _proto = Point.prototype;
+
+    _proto.add = function add(point) {
+      return new Point(this.x + point.x, this.y + point.y);
+    };
+
+    _proto.smult = function smult(value) {
+      return new Point(this.x * value, this.y * value);
+    };
+
+    _proto.vscale = function vscale(scale, origin) {
+      if (origin === void 0) {
+        origin = new Point(0, 0);
+      }
+
+      return new Point(origin.x + (this.x - origin.x) * scale.x, origin.y + (this.y - origin.y) * scale.y);
+    };
+
+    _proto.sscale = function sscale(scale, origin) {
+      if (origin === void 0) {
+        origin = new Point(0, 0);
+      }
+
+      return this.vscale(new Point(scale, scale), origin);
+    };
+
+    return Point;
+  }();
+  var Size = /*#__PURE__*/function () {
+    function Size(width, height) {
+      this.width = round(width);
+      this.height = round(height);
+    }
+
+    var _proto2 = Size.prototype;
+
+    _proto2.fitSize = function fitSize(ratio) {
+      var width;
+      var height; //
+
+      if (this.ratio > ratio) {
+        height = this.height;
+        width = height * ratio;
+      } //
+      else {
+          width = this.width;
+          height = width / ratio;
+        }
+
+      return new Size(width, height);
+    };
+
+    _createClass(Size, [{
+      key: "ratio",
+      get: function get() {
+        return this.width / this.height;
+      }
+    }]);
+
+    return Size;
+  }();
+  var Rectangle = /*#__PURE__*/function () {
+    function Rectangle(origin, size) {
+      this.origin = origin;
+      this.size = size;
+    } // Simple shortcuts
+
+
+    var _proto3 = Rectangle.prototype;
+
+    //
+    _proto3.translate = function translate(vector) {
+      return new Rectangle(this.origin.add(vector), this.size);
+    };
+
+    _proto3.fitRectangleCenter = function fitRectangleCenter(ratio) {
+      // Getting the base size
+      var size = this.size.fitSize(ratio); // Then we calculate its new x & y
+
+      var x = this.origin.x + (this.width - size.width) / 2;
+      var y = this.origin.y + (this.height - size.height) / 2; //
+
+      return new Rectangle(new Point(x, y), size);
+    };
+
+    _createClass(Rectangle, [{
+      key: "x",
+      get: function get() {
+        return this.origin.x;
+      }
+    }, {
+      key: "y",
+      get: function get() {
+        return this.origin.y;
+      }
+    }, {
+      key: "width",
+      get: function get() {
+        return this.size.width;
+      }
+    }, {
+      key: "height",
+      get: function get() {
+        return this.size.height;
+      }
+    }, {
+      key: "ratio",
+      get: function get() {
+        return this.size.ratio;
+      }
+    }]);
+
+    return Rectangle;
+  }();
+  var Cell = /*#__PURE__*/function (_Rectangle) {
+    _inheritsLoose(Cell, _Rectangle);
+
+    function Cell(origin, size, index) {
+      var _this;
+
+      _this = _Rectangle.call(this, origin, size) || this;
+      _this.index = index;
+      return _this;
+    }
+
+    _createClass(Cell, [{
+      key: "i",
+      get: function get() {
+        return this.index.i;
+      }
+    }, {
+      key: "j",
+      get: function get() {
+        return this.index.j;
+      }
+    }]);
+
+    return Cell;
+  }(Rectangle);
+  var UnitGrid = /*#__PURE__*/function () {
+    function UnitGrid(rows, columns, cell_ratio, spacing) {
+      if (cell_ratio === void 0) {
+        cell_ratio = 1;
+      }
+
+      if (spacing === void 0) {
+        spacing = {
+          column: 0,
+          row: 0
+        };
+      }
+
+      this.rows = rows;
+      this.columns = columns;
+      this.cell_ratio = cell_ratio;
+      this.spacing = spacing;
+    }
+
+    var _proto4 = UnitGrid.prototype;
+
+    _proto4.getCellHeightFromGridHeight = function getCellHeightFromGridHeight(height) {
+      return height / this.height;
+    };
+
+    _proto4.getCellHeightFromGridWidth = function getCellHeightFromGridWidth(width) {
+      return width / this.width;
+    };
+
+    _proto4.getCells = function getCells(cell_height, translation) {
+      if (cell_height === void 0) {
+        cell_height = 1;
+      }
+
+      if (translation === void 0) {
+        translation = new Point(0, 0);
+      }
+
+      var cells = [];
+
+      for (var r = 0; r < this.rows; r++) {
+        var y = r * (1 + this.spacing.row);
+
+        for (var c = 0; c < this.columns; c++) {
+          var x = c * (this.cell_ratio + this.spacing.column);
+          var origin = new Point(x, y).sscale(cell_height).add(translation);
+          cells.push(new Cell(origin, new Size(cell_height * this.cell_ratio, cell_height), {
+            i: r,
+            j: c
+          }));
+        }
+      }
+
+      return cells;
+    };
+
+    _createClass(UnitGrid, [{
+      key: "height",
+      get: function get() {
+        /**
+         * number of cells in a column   * cell height +
+         * number of gaps between colums * gap height
+         */
+        return this.rows * 1 + (this.rows - 1) * this.spacing.row;
+      }
+    }, {
+      key: "width",
+      get: function get() {
+        /**
+         * number of cell in row        * cell width +
+         * number of gaps between rows  * gap width
+         */
+        return this.columns * this.cell_ratio + (this.columns - 1) * this.spacing.column;
+      }
+    }, {
+      key: "ratio",
+      get: function get() {
+        return this.width / this.height;
+      }
+    }]);
+
+    return UnitGrid;
+  }();
+
+  exports.Cell = Cell;
+  exports.Point = Point;
+  exports.Rectangle = Rectangle;
+  exports.Size = Size;
+  exports.UnitGrid = UnitGrid;
+
+})));
 //# sourceMappingURL=index.umd.js.map
